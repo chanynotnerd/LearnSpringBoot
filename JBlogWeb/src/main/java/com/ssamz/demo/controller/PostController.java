5e5190da678f35c56020f0a3210b868335ed52cb
chanynotnerd <63177308+chanynotnerd@users.noreply.github.com>
2023-11-22 00:13:45 +0900

    Apply WebJar and design index with jsp files/w3school

---
+++ JBlogWeb/src/main/java/com/ssamz/demo/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.ssamz.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostController {
+	@GetMapping({"/jblog/view"})	// RESTController의 GetMapping과 겹쳐서 jsp파일이 나오지 않아 수정. 원래는 @GetMapping({"", "/"})
+	public String getPostList() 
+	{
+		return "index";	
+	}
+}
