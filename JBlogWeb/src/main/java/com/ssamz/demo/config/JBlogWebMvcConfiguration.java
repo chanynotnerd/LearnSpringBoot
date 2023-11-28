dedce89abfb77a69d7927433eedf3b712477feb8
chanynotnerd <63177308+chanynotnerd@users.noreply.github.com>
2023-11-28 23:06:50 +0900

    Apply language system with ko, en

--- JBlogWeb/src/main/java/com/ssamz/demo/config/JBlogWebMvcConfiguration.java
+++ JBlogWeb/src/main/java/com/ssamz/demo/config/JBlogWebMvcConfiguration.java
@@ -1,22 +1,30 @@
 package com.ssamz.demo.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 
 @Configuration
 public class JBlogWebMvcConfiguration implements WebMvcConfigurer{
+	
+	
+	
 	@Bean("messageSource")
 	public MessageSource messageSource()
 	{
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename("message/messageSource");
+		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
 	}
 	
@@ -31,10 +39,32 @@ public class JBlogWebMvcConfiguration implements WebMvcConfigurer{
 		return new ModelMapper();
 	}
 	
+	@Bean
+    public FilterRegistrationBean<CharacterEncodingFilter> characterEncodingFilterRegistration() {
+        FilterRegistrationBean<CharacterEncodingFilter> registrationBean = new FilterRegistrationBean<>();
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns("/*"); // 모든 경로에 대해 필터 적용
+        return registrationBean;
+    }
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() 
+	{
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry)
 	{
 		registry.addInterceptor(
 				new AuthenticateInterceptor()).addPathPatterns("/", "/post/**");
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 }
--- JBlogWeb/src/main/webapp/WEB-INF/jblog/layout/footer.jsp
+++ JBlogWeb/src/main/webapp/WEB-INF/jblog/layout/footer.jsp
@@ -4,6 +4,8 @@
 		<p>Create by Chanynotnerd</p>
 		<p>연락처: 010-2044-1850, 주소지: 서울시 강남구</p>
 		<p>designed by w3school/BS5</p>
+		<a href="/auth/login?lang=ko"><img height="38px" src="/image/korean.png"></a>&nbsp;&nbsp;
+		<a href="/auth/login?lang=en"><img height="38px" src="/image/english.png"></a>
 	</div>
 </body>
 <body>
