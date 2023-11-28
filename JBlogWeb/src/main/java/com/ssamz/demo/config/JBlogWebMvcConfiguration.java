a337615f672dc420c9ea055a492186355f6cfb0b
chanynotnerd <63177308+chanynotnerd@users.noreply.github.com>
2023-11-28 16:59:03 +0900

    still apply properties about languages

--- JBlogWeb/src/main/java/com/ssamz/demo/config/JBlogWebMvcConfiguration.java
+++ JBlogWeb/src/main/java/com/ssamz/demo/config/JBlogWebMvcConfiguration.java
@@ -1,13 +1,31 @@
 package com.ssamz.demo.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class JBlogWebMvcConfiguration implements WebMvcConfigurer{
+	@Bean("messageSource")
+	public MessageSource messageSource()
+	{
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("message/messageSource");
+		return messageSource;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver()
+	{
+		return new SessionLocaleResolver();
+	}
+	
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
